717b6e8013a03106ed5c9c9f5f59e5eaa15bdb16
unknown <youneskazed750@gmail.com>
2025-10-28 21:51:35 +0100

    verion 1.1.0

--- test.py
+++ test.py
@@ -2,11 +2,9 @@ version = "1.0.3"
 commands = ["open calc", "say hello", "kill ousama"]
 
 
-
 print(f"you are running {version}")
 
 usr_input = input("what do you want to do: ")
-print(usr_input)
 if usr_input in commands:
     print(f"commiting command {usr_input}...")
 else:
