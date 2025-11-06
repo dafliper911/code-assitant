60f8fc009cb9a8227259aef61faf5200e58dab96
unknown <youneskazed750@gmail.com>
2025-11-06 21:48:14 +0100

    3.1.0

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-version = "3.0.0"  # major.minor.patch
-commands = ["open calc", "say hello", "kill ousama"]
+import os
 
+
+version = "3.1.0"  # major.minor.patch
+commands = ["open calc", "say hello", "kill ousama"]
 
 print(f"you are running {version}")
 
@@ -9,3 +11,8 @@ if usr_input in commands:
     print(f"commiting command {usr_input}...")
 else:
     print("invalid command")
+if usr_input == "open calc":
+
+    os.system("calc.exe")
+if usr_input == "say hello":
+    print("hello user!")
