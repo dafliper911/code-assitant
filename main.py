639889977726094a41d7236da365328d5f0f5da8
unknown <youneskazed750@gmail.com>
2025-11-09 12:40:00 +0100

    3.3.0

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 
 
-version = "3.2.0"  # major.minor.patch
-commands = ["open calc", "say hello", "kill ousama"]
+version = "3.3.0"  # major.minor.patch
+commands = ["open calc", "say hello", "kill ousama", "exit", "help"]
 
 print(f"you are running {version}")
 
-usr_input = input("what do you want to do: ")
+usr_input = input("enter command: ")
 if usr_input in commands:
     print(f"commiting command {usr_input}...")
 else:
@@ -17,3 +17,10 @@ if usr_input == "say hello":
     print("hello user!")
 if usr_input == "kill ousama":
     print("ousama bin laden was sucsessfully assasinated")
+if usr_input == "exit":
+    print("exiting...")
+    exit()
+if usr_input == "help":
+    print("available commands:")
+    for command in commands:
+        print(f"- {command}")
